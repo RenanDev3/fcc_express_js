ccd7529d036e5c030d3a205233025d3a892741b5
Renan Boaventura <renandev@outlook.com>
2023-09-26 17:16:33 -0300

    implement the routes for the project

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,22 @@
-console.log('Task Manager App')
+const express = require("express")
+const tasks = require("./routes/tasks")
+
+const app = express()
+
+// middleware
+
+app.use(express.json())
+
+
+// routes
+app.get('/hello', (request, response) => {
+    response.send("Task manager app")
+})
+
+
+app.use("/api/v1/tasks", tasks)
+
+
+const port = 3333
+
+app.listen(port, console.log(`Server is listening on port ${port}`))
